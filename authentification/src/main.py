073bca7fc1f18acbea8a09ce80ebb2f5fa949446
Malak OUJDID <164894637+Asuraa3@users.noreply.github.com>
2025-01-22 21:42:39 +0100

    Revert "Correction des bugs empêchant le bon fonctionnement de l'API"

--- authentification/src/main.py
+++ authentification/src/main.py
@@ -3,11 +3,9 @@ import os
 from dotenv import load_dotenv
 
 from config import app
-from routes import route
+
 load_dotenv()
 
 if __name__ == "__main__":
     debug_mode = os.getenv("FLASK_DEBUG", "false").lower() == "true"
-    app.register_blueprint(route)
-    app.run(host="0.0.0.0", debug=debug_mode, port=5000)
-
+    app.run(debug=debug_mode, port=5000)
